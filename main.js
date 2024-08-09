a26e82082f39e953aea944eed9390033c3c286ea
Ale-M94 <175752593+Ale-M94@users.noreply.github.com>
2024-08-09 10:08:01 -0300

    Correct human choice function to make it insensitive. Get the function to play a single round.
    The function getHumanChoice has been modified so the user can type his choice, and the result is case insensitive.
    Also it has been added the function to play a single round and show the score

--- main.js
+++ main.js
@@ -6,24 +6,52 @@ function getRandomInt(a){
 function getComputerChoice(){
     let choice = getRandomInt(3);
     if (choice === 0){
-        return "Rock";
+        return "rock";
     }else if (choice === 1){
-        return "Paper";
+        return "paper";
     }else if (choice === 2){
-        return "Scissors";
+        return "scissors";
     }
 }
 
-//Function to make the human choice
+//Function to make the human choice case insensitive
 function getHumanChoice(){
-    let choice = parseInt(prompt("Choose your option:\n 1. Rock\n 2. Paper\n 3. Scissors"));
-    if (choice === 1){
-        return "Rock";
-    }else if (choice === 2){
-        return "Paper";
-    }else if (choice === 3){
-        return "Scissors";
-    }
+    let choice = prompt("Choose your option:").toLowerCase();
+    if (choice !== "rock" && "paper" && "scissors"){
+        return 'Invalid option, please choose "Rock", "Paper" or "Scissors"'; 
+    }else
+    return choice;
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors.");
+        humanScore = humanScore + 1;
+    }else if (humanChoice == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock.");
+        computerScore = computerScore + 1;
+    }else if (humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You win! Paper beats Rock.");
+        humanScore = humanScore + 1;
+    }else if (humanChoice == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper.");
+        computerScore = computerScore + 1;
+    }else if (humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats Paper.");
+        humanScore = humanScore + 1;
+    }else if (humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors.");
+        computerScore = computerScore + 1;
+    }else if (humanChoice == computerChoice){
+        console.log("It's a tie!");
+    }
+    console.log("El resultado es:\n Computadora: " + computerScore + "\n Jugador: " + humanScore);    
+}
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
